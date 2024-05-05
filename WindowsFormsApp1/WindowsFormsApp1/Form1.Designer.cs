467732b1249fe5bf187a38c5816cd491c369b9ad
Assami <minimiddo14@gmail.com>
2024-05-06 00:41:17 +0500

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(30, 453);
+            this.maskedTextBox1.Mask = "00000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(176, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 437);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Кол-во текстов";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 479);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 61);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(258, 349);
+            this.textBox1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(326, 576);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Входжения слов";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(353, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 576);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Обход сайта";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(30, 469);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(176, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(30, 429);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(176, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 455);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Максимальная глубина";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 61);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(258, 349);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 499);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 413);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Адрес сайта";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox8);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.textBox7);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.textBox6);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(685, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(326, 576);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "СЛАУ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(30, 61);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(258, 247);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 490);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 47);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Матрица коэффициентов";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(30, 333);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(138, 102);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(30, 454);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(138, 20);
+            this.textBox7.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 438);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Свободные члены";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(184, 453);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(89, 20);
+            this.textBox8.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(181, 437);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Eps";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 600);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace WindowsFormsApp1
+{
+
+    
+    public partial class Form1 : Form
+    {
+        private HttpClient _client = new HttpClient();
+        private Stopwatch _stopwatch = new Stopwatch();
+        private double[,] matrix;
+        private double[] freeTerms;
+        private double[] currentSolution;
+        private double eps;
+        private int size;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Click += async (sender, e) => await StartCrawling();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Деактивация GUI элементов, чтобы избежать повторного нажатия
+                button1.Enabled = false;
+                maskedTextBox1.Enabled = false;
+
+                if (!int.TryParse(maskedTextBox1.Text, out int numberOfTexts))
+                {
+                    MessageBox.Show("Введите корректное число в поле 'Кол-во текстов'");
+                    return;
+                }
+
+                var texts = GenerateTexts(numberOfTexts);
+                var wordsToFind = new List<string> { "example", "text", "word" };
+                var wordCounts = new ConcurrentDictionary<string, int>();
+
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                // Асинхронный запуск задачи с использованием Task.Run
+                await Task.Run(() =>
+                {
+                    // Используем Parallel.ForEach для параллельной обработки
+                    Parallel.ForEach(texts, () => new Dictionary<string, int>(),
+                        (text, loopState, localWordCount) =>
+                        {
+                            foreach (var word in wordsToFind)
+                            {
+                                int count = CountWordOccurrences(text, word);
+                                if (localWordCount.ContainsKey(word))
+                                    localWordCount[word] += count;
+                                else
+                                    localWordCount.Add(word, count);
+                            }
+                            return localWordCount;
+                        },
+                        localWordCount =>
+                        {
+                            foreach (var pair in localWordCount)
+                            {
+                                wordCounts.AddOrUpdate(pair.Key, pair.Value, (key, oldValue) => oldValue + pair.Value);
+                            }
+                        }
+                    );
+                });
+
+                stopwatch.Stop();
+
+                // Обновление интерфейса пользователя в основном потоке
+                textBox1.Text = $"Processed texts: {numberOfTexts}\r\nProcessing Time: {stopwatch.ElapsedMilliseconds} ms\r\n";
+                foreach (var word in wordsToFind)
+                {
+                    textBox1.Text += $"Word: {word}, Count: {wordCounts[word]}\r\n";
+                }
+            }
+            finally
+            {
+                // Реактивация GUI элементов
+                button1.Enabled = true;
+                maskedTextBox1.Enabled = true;
+            }
+        }
+
+        // Ваши вспомогательные методы здесь
+        private List<string> GenerateTexts(int numberOfTexts)
+        {
+            var baseTexts = new List<string>
+    {
+        "This is a simple example text containing some example words.",
+        "Another text, which is used to test the occurrence count of specific words.",
+        "The quick brown fox jumps over the lazy dog."
+    };
+
+            var texts = new List<string>();
+            for (int i = 0; i < numberOfTexts; i++)
+            {
+                texts.AddRange(baseTexts);
+            }
+            return texts;
+        }
+
+        private int CountWordOccurrences(string text, string word)
+        {
+            int count = 0, startIndex = 0;
+            while ((startIndex = text.IndexOf(word, startIndex, StringComparison.OrdinalIgnoreCase)) != -1)
+            {
+                count++;
+                startIndex += word.Length;
+            }
+            return count;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private async Task StartCrawling()
+        {
+            string url = textBox4.Text;
+            int maxDepth = int.Parse(textBox5.Text);
+            Node root = new Node(url);
+
+            _stopwatch.Restart();
+            await CrawlLevel(root, maxDepth);
+            _stopwatch.Stop();
+        }
+
+        private async Task CrawlLevel(Node root, int maxDepth)
+        {
+            List<Node> currentLevel = new List<Node> { root };
+            int currentDepth = 0;
+
+            while (currentDepth <= maxDepth && currentLevel.Count > 0)
+            {
+                List<Task> tasks = new List<Task>();
+                List<Node> nextLevel = new List<Node>();
+
+                foreach (var node in currentLevel)
+                {
+                    tasks.Add(Task.Run(async () =>
+                    {
+                        Uri baseUri = new Uri(node.Url);
+                        var response = await _client.GetAsync(baseUri);
+                        var pageContents = await response.Content.ReadAsStringAsync();
+                        var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+                        htmlDoc.LoadHtml(pageContents);
+
+                        var links = htmlDoc.DocumentNode.SelectNodes("//a[@href]");
+                        if (links != null)
+                        {
+                            foreach (var link in links)
+                            {
+                                string hrefValue = link.GetAttributeValue("href", string.Empty);
+                                Uri fullUri = new Uri(baseUri, hrefValue);
+                                var childNode = new Node(fullUri.ToString());
+                                node.Children.Add(childNode);
+                                nextLevel.Add(childNode);
+                            }
+                        }
+                    }));
+                }
+
+                await Task.WhenAll(tasks);
+                PrintTreeInfo(root, currentDepth);
+                currentLevel = nextLevel;
+                currentDepth++;
+            }
+        }
+
+        private void PrintTreeInfo(Node root, int depth)
+        {
+            int height = GetHeight(root);
+            int totalNodes = GetTotalNodes(root);
+            var leaves = GetLeaves(root, 2, depth); // Получаем только два листа на текущей глубине
+
+            // Если мы находимся на глубине 0 и список листьев пуст, используем URL корня
+            if (depth == 0 && leaves.Count == 0)
+            {
+                leaves.Add(root.Url);
+                foreach (var child in root.Children)
+                {
+                    leaves.Add(child.Url);
+                    if (leaves.Count >= 2) break;
+                }
+            }
+
+            textBox2.Invoke(new Action(() =>
+            {
+                textBox2.AppendText($"Глубина обработки завершена: {depth}, Время выполнения: {_stopwatch.ElapsedMilliseconds} мс, Высота дерева: {height}, Количество узлов: {totalNodes}, Пример листьев на глубине {depth}: {string.Join(", ", leaves)}\n");
+            }));
+            _stopwatch.Restart(); // Перезапуск таймера для следующего уровня
+        }
+
+        private int GetHeight(Node node)
+        {
+            int height = 1;
+            foreach (var child in node.Children)
+            {
+                height = Math.Max(height, 1 + GetHeight(child));
+            }
+            return height;
+        }
+
+        private int GetTotalNodes(Node node)
+        {
+            int count = 1;
+            foreach (var child in node.Children)
+            {
+                count += GetTotalNodes(child);
+            }
+            return count;
+        }
+
+        private List<string> GetLeaves(Node node, int maxLeaves, int targetDepth, int currentDepth = 0)
+        {
+            List<string> leaves = new List<string>();
+            if (currentDepth == targetDepth)
+            {
+                if (node.Children.Count == 0) // Это лист на целевой глубине
+                {
+                    leaves.Add(node.Url);
+                }
+            }
+            else
+            {
+                foreach (var child in node.Children)
+                {
+                    leaves.AddRange(GetLeaves(child, maxLeaves - leaves.Count, targetDepth, currentDepth + 1));
+                    if (leaves.Count >= maxLeaves) break;
+                }
+            }
+            return leaves.Take(maxLeaves).ToList();
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            ReadInput();
+            await Task.Run(() => SolveJacobi());
+        }
+
+        private void ReadInput()
+        {
+            // Чтение и парсинг матрицы коэффициентов из TextBox
+            string[] matrixLines = textBox6.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            size = matrixLines.Length; // Размер матрицы определяется количеством строк
+            matrix = new double[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                string[] elements = matrixLines[i].Trim().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = double.Parse(elements[j]);
+                }
+            }
+
+            // Чтение вектора свободных членов
+            string[] freeTermsEntries = textBox7.Text.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            freeTerms = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                freeTerms[i] = double.Parse(freeTermsEntries[i]);
+            }
+
+            // Чтение значения точности
+            eps = double.Parse(textBox8.Text);
+
+            // Инициализация вектора текущего решения
+            currentSolution = new double[size];
+        }
+
+
+        private void SolveJacobi()
+        {
+            double[] newSolution = new double[size];
+            Thread[] threads = new Thread[size];
+            double norm;
+
+            do
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    int index = i;
+                    threads[i] = new Thread(() => SolveElement(index, newSolution));
+                    threads[i].Start();
+                }
+
+                // Ждем завершения всех потоков
+                foreach (Thread thread in threads)
+                {
+                    thread.Join();
+                }
+
+                norm = ComputeNorm(currentSolution, newSolution);
+                Array.Copy(newSolution, currentSolution, size);
+            } while (norm > eps);
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                textBox3.AppendText($"Решение найдено с точностью {eps}\r\n");
+                foreach (var val in currentSolution)
+                    textBox3.AppendText($"{val:F4}\r\n");
+            });
+        }
+
+        private void SolveElement(int index, double[] newSolution)
+        {
+            double sum = freeTerms[index];
+            for (int j = 0; j < size; j++)
+            {
+                if (index != j)
+                {
+                    sum -= matrix[index, j] * currentSolution[j];
+                }
+            }
+            newSolution[index] = sum / matrix[index, index];
+        }
+
+        private double ComputeNorm(double[] oldSolution, double[] newSolution)
+        {
+            double norm = 0.0;
+            for (int i = 0; i < oldSolution.Length; i++)
+            {
+                norm += Math.Pow(newSolution[i] - oldSolution[i], 2);
+            }
+            return Math.Sqrt(norm);
+        }
+    }
+
+    public class Node
+    {
+        public string Url { get; set; }
+        public List<Node> Children { get; set; } = new List<Node>();
+
+        public Node(string url)
+        {
+            Url = url;
+        }
+    }
+
+    
+}
